834ec9eb1b376d42f64e9ceae2b55c64657961ca
Landry Kapela <landrykapela@gmail.com>
2021-02-08 18:32:00 +0300

    Add schedule and activity forms

--- js/main.js
+++ js/main.js
@@ -33,6 +33,51 @@ if (window.location.pathname.includes("/admin.html")) {
 const initializeSchedule = () => {
   var dp = new DayPilot.Calendar("schedule");
   dp.viewType = "Week";
+  dp.scale = "Manual";
+  dp.theme = "calendar_g";
+  dp.timeline = [];
+  for (var i = 0; i < 24; i++) {
+    var day = {};
+    day.start = dp.startDate.addDays(i);
+    day.end = day.start.addDays(1);
+    dp.timeline.push(day);
+  }
+
+  dp.events.list = calEvents.map((e) => {
+    let event = e;
+    event.text = e.activity;
+    event.start = e.date + "T" + e.startTime;
+    event.end = e.date + "T" + e.endTime;
+    return event;
+  });
+  dp.onEventClicked = (args) => {
+    let details = args.e.data;
+    let crew = details.crew.map((c) => {
+      let d = c.name + " (" + c.role + ")";
+      return d;
+    });
+    let crewText = "";
+    crew.forEach((c, i) => {
+      crewText +=
+        i +
+        1 +
+        ". " +
+        c +
+        ` 
+
+        `;
+    });
+    let eDate =
+      details.date + " from " + details.startTime + " to " + details.endTime;
+    // detailText += "\n" + "Notes:\n" + details.notae;
+    let modal = $("#modal1");
+    modal.find(".modal-title").text(details.text);
+    modal.find("#event-crew").text(crewText);
+    modal.find("#event-date").text(eDate);
+    modal.modal("show");
+
+    $modalBody = document.querySelector("#event-details");
+  };
   dp.init();
 };
 if (window.location.pathname.includes("/dashboard.html")) {
@@ -40,6 +85,7 @@ if (window.location.pathname.includes("/dashboard.html")) {
     let hash = window.location.hash;
     if (!hash.length) hash = "#all_activity";
     showContent(hash);
+    if (hash.substr(1) === "schedule") initializeSchedule();
   });
   window.addEventListener("hashchange", () => {
     let hash = window.location.hash;
