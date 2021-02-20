ce64f26c73679de120966c2f7ffdcec9aea1ce0a
Landry Kapela <landrykapela@gmail.com>
2021-02-20 17:54:38 +0300

    add localstorage data

--- js/data.js
+++ js/data.js
@@ -1,8 +1,182 @@
+const departments = [
+  { id: 0, name: "News", manager: 1 },
+  { id: 1, name: "Production", manager: 2 },
+  { id: 2, name: "UTV", manager: 3 },
+  { id: 3, name: "Sports", manager: 4 },
+  { id: 4, name: "Technical", manager: 5 },
+  { id: 5, name: "Transport", manager: 6 },
+  { id: 6, name: "Administration", manager: 7 },
+];
+
+//units
+const units = [
+  { id: 0, name: "Current Events", deptId: 0, hod: 10 },
+  { id: 1, name: "International News", deptId: 0, hod: 11 },
+  { id: 2, name: "Visual Editing", deptId: 1, hod: 12 },
+  { id: 3, name: "Camera", deptId: 1, hod: 13 },
+  { id: 4, name: "Switching", deptId: 1, hod: 14 },
+  { id: 5, name: "Programs", deptId: 2, hod: 15 },
+  { id: 6, name: "Live Events", deptId: 3, hod: 16 },
+  { id: 7, name: "Engineering", deptId: 4, hod: 17 },
+  { id: 8, name: "Broadcasting", deptId: 4, hod: 18 },
+];
+
+//users
+const users = [
+  {
+    id: 0,
+    name: "System Admin",
+    position: "Administrator",
+    deptId: 5,
+    department: "Administration",
+  },
+  {
+    id: 1,
+    name: "Salma Bakari",
+    position: "Manager",
+    deptId: 0,
+    department: "News",
+  },
+  {
+    id: 2,
+    name: "Alfred Mahenge",
+    position: "Manager",
+    deptId: 1,
+    department: "Production",
+  },
+  {
+    id: 3,
+    name: "Xavier Banda",
+    position: "Manager",
+    deptId: 2,
+    department: "UTV",
+  },
+  {
+    id: 4,
+    name: "Abdulaziz Juma",
+    position: "Manager",
+    deptId: 3,
+    department: "Sports",
+  },
+  {
+    id: 5,
+    name: "Joyce Mzugu",
+    position: "Manager",
+    deptId: 4,
+    department: "Technical",
+  },
+  {
+    id: 20,
+    name: "Juma Ali",
+    position: "Camera Operator",
+    deptId: 1,
+    department: "Production",
+  },
+  {
+    id: 21,
+    name: "Fatma Mohammed",
+    position: "Camera Operator",
+    deptId: 1,
+    department: "Production",
+  },
+  {
+    id: 22,
+    name: "Jackson John",
+    position: "Camera Operator",
+    deptId: 1,
+    department: "Production",
+  },
+  {
+    id: 23,
+    name: "Mustafa Ayub",
+    position: "Camera Operator",
+    deptId: 1,
+    department: "Production",
+  },
+  {
+    id: 24,
+    name: "Catherine Peter",
+    position: "Camera Operator",
+    deptId: 1,
+    department: "Production",
+  },
+  {
+    id: 25,
+    name: "John Mamboleo",
+    position: "Camera Operator",
+    deptId: 1,
+    department: "Production",
+  },
+  {
+    id: 30,
+    name: "Alex Muba",
+    position: "Switcher",
+    deptId: 1,
+    department: "Production",
+  },
+  {
+    id: 31,
+    name: "Martha Kalokola",
+    position: "Switcher",
+    deptId: 1,
+    department: "Production",
+  },
+  {
+    id: 32,
+    name: "Abuu Semainda",
+    position: "Switcher",
+    deptId: 1,
+    department: "Production",
+  },
+  {
+    id: 33,
+    name: "Joyce Konda",
+    position: "Switcher",
+    deptId: 1,
+    department: "Production",
+  },
+  {
+    id: 40,
+    name: "Joyce Banzi",
+    position: "Visual Editor",
+    deptId: 1,
+    department: "Production",
+  },
+  {
+    id: 41,
+    name: "Mary Banzi",
+    position: "Visual Editor",
+    deptId: 1,
+    department: "Production",
+  },
+  {
+    id: 42,
+    name: "Fauzia Ali",
+    position: "Visual Editor",
+    deptId: 1,
+    department: "Production",
+  },
+  {
+    id: 43,
+    name: "Athuman Mbaruku",
+    position: "Visual Editor",
+    deptId: 1,
+    department: "Production",
+  },
+  {
+    id: 50,
+    name: "Francis Kiboko",
+    position: "Engineer",
+    deptId: 4,
+    department: "Technical",
+  },
+];
+
 const calEvents = [
   {
     id: 0,
     activity: "Morning Trumpet Special",
-    date: "2021-02-07",
+    date: "2021-02-12",
     startTime: "06:00:00",
     endTime: "09:00:00",
     crew: [
@@ -18,7 +192,7 @@ const calEvents = [
   {
     id: 1,
     activity: "Adhuhuri Live",
-    date: "2021-02-07",
+    date: "2021-02-11",
     startTime: "13:00:00",
     endTime: "16:00:00",
     crew: [
@@ -34,7 +208,7 @@ const calEvents = [
   {
     id: 2,
     activity: "Alasiri Lounge",
-    date: "2021-02-07",
+    date: "2021-02-11",
     startTime: "16:00:00",
     endTime: "19:00:00",
     crew: [
@@ -50,7 +224,7 @@ const calEvents = [
   {
     id: 3,
     activity: "Habari",
-    date: "2021-02-07",
+    date: "2021-02-13",
     startTime: "20:00:00",
     endTime: "22:00:00",
     crew: [
@@ -64,57 +238,155 @@ const calEvents = [
     note: "Live studio event",
   },
 ];
+//dummy assignments
+const dummyAss = [];
 
+//dummy resources
+const dummyResources = [
+  {
+    id: "camera",
+    name: "Camera Operators",
+    expanded: true,
+    children: [
+      { id: "0", name: "Ali Juma" },
+      { id: "1", name: "Ernesta John" },
+      { id: "2", name: "Emmanuel Mbogo" },
+      { id: "3", name: "Yahya Abdallah" },
+      { id: "4", name: "Yusta Kalikawe" },
+      { id: "5", name: "James Magongo" },
+    ],
+  },
+  {
+    id: "sound",
+    name: "Sound Engineers",
+    expanded: true,
+    children: [
+      { id: "1", name: "Ravinda Kumar" },
+      { id: "2", name: "Jumanne Kasongo" },
+      { id: "3", name: "Johnson Peter" },
+      { id: "4", name: "Experius Mponda" },
+    ],
+  },
+  {
+    id: "producer",
+    name: "Producers",
+    expanded: true,
+    children: [
+      { id: "1", name: "Fatma Mmari" },
+      { id: "2", name: "Albogast Thobias" },
+    ],
+  },
+  {
+    id: "presenter",
+    name: "Presenters",
+    expanded: true,
+    children: [
+      { id: "1", name: "Nurueli Samson" },
+      { id: "2", name: "Zuhura Marijani" },
+      { id: "3", name: "Neema Mushi" },
+      { id: "4", name: "Edwin Paul" },
+    ],
+  },
+  {
+    id: "switcher",
+    name: "Switchers",
+    expanded: true,
+    children: [
+      { id: "1", name: "Oscar Mabele" },
+      { id: "2", name: "Athumani Majamba" },
+    ],
+  },
+  {
+    id: "visual",
+    name: "Visual Editors",
+    expanded: true,
+    children: [
+      { id: "1", name: "Mohammed Bakari" },
+      { id: "2", name: "Henri Mtawali" },
+      { id: "1", name: "Eunice Kamala" },
+      { id: "2", name: "Mwanaidi Mponzi" },
+    ],
+  },
+  {
+    id: "electrical",
+    name: "Electrical Engineers",
+    expanded: true,
+    children: [
+      { id: 1, name: "Johnson John" },
+      { id: 1, name: "Denis Mungereza" },
+    ],
+  },
+  {
+    id: "lighting",
+    name: "Lighting Engineers",
+    children: [
+      { id: 1, name: "Adonis Alphonce" },
+      { id: 1, name: "Bakari Juma" },
+    ],
+  },
+];
 //dummy activities
-const activities = [
+const activitiesData = [
   {
     id: 0,
     status: "Open",
-    name: "Simba vs Azam Live Match",
+    name: "JKT Oljoiro vs Azam Live Match",
     nature: "Unscheduled",
     department: "Sports",
-    region: "Dar es Salaam",
-    venue: "Chamazi Stadium",
-    date: "2021-02-12",
-    startTime: "16:00:00",
-    endTime: "18:00:00",
-    resources: [
+    region: "Arusha",
+    venue: "Chuga Stadium",
+    date: "2021-02-13",
+    startTime: "2021-02-10T6:00:00",
+    endTime: "2021-02-12T18:00:00",
+    resource: [
       {
-        camera: ["CO1", "CO2", "CO3", "CO4", "CO5", "CO6"],
-        sound: ["SE1", "SE2"],
-        producer: ["PR1"],
-        presenter: ["PS1", "PS2"],
-        switcher: ["ST1", "ST2"],
-        visual: ["VE1"],
-        electrical: ["EE1"],
-        lighting: ["LE1"],
-        note: "Great Match",
+        id: "camera",
+        name: "Camera Operators",
+        children: ["0", "2", "3", "4", "5", "6"],
       },
+      {
+        id: "sound",
+        name: "Sound Engineers",
+        children: ["1", "2"],
+      },
+      { id: "producer", name: "Producers", children: ["1"] },
+      { id: "presenter", name: "Presenters", children: ["1", "2"] },
+      { id: "switcher", name: "Switchers", children: ["1", "2"] },
+      { id: "visual", name: "Visual Editors", children: ["1"] },
+      { id: "electrical", name: "Electrical Engineers", children: [1] },
+      { id: "lighting", name: "Lighting Engineers", children: [1] },
     ],
+    note: "Great Match",
   },
   {
     id: 1,
     status: "Open",
-    name: "Simba vs Azam Live Match",
+    name: "Interview with President JP Magufuli",
     nature: "Unscheduled",
-    department: "Sports",
+    department: "UTV",
     region: "Dar es Salaam",
-    venue: "Chamazi Stadium",
-    date: "2021-02-12",
-    startTime: "16:00:00",
-    endTime: "18:00:00",
-    resources: [
+    venue: "Ikulu",
+    date: "2021-02-14",
+    startTime: "2021-02-14T11:00:00",
+    endTime: "2021-02-14T12:00:00",
+    resource: [
       {
-        camera: ["CO1", "CO2", "CO3", "CO4", "CO5", "CO6"],
-        sound: ["SE1", "SE2"],
-        producer: ["PR1"],
-        presenter: ["PS1", "PS2"],
-        switcher: ["ST1", "ST2"],
-        visual: ["VE1"],
-        electrical: ["EE1"],
-        lighting: ["LE1"],
-        note: "Great Match",
+        id: "camera",
+        name: "Camera Operators",
+        children: ["0", "2", "3", "4", "5", "6"],
       },
+      {
+        id: "sound",
+        name: "Sound Engineers",
+        children: ["1", "2"],
+      },
+      { id: "producer", name: "Producers", children: ["1"] },
+      { id: "presenter", name: "Presenters", children: ["1", "2"] },
+      { id: "switcher", name: "Switchers", children: ["1", "2"] },
+      { id: "visual", name: "Visual Editors", children: ["1"] },
+      { id: "electrical", name: "Electrical Engineers", children: [1] },
+      { id: "lighting", name: "Lighting Engineers", children: [1] },
     ],
+    note: "Presidential Event",
   },
 ];
--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+const localStorage = window.localStorage;
+const units = JSON.parse(localStorage.units);
+const positions = JSON.parse(localStorage.positions);
+const departments = JSON.parse(localStorage.departments);
+const users = JSON.parse(localStorage.users);
+
 const loginForm = document.querySelector("#login-form");
 if (loginForm) {
   loginForm.addEventListener("submit", (e) => {
@@ -5,6 +11,58 @@ if (loginForm) {
     window.location.pathname = "/admin.html";
   });
 }
+
+//populate select with
+const populate = (id, data, dependant) => {
+  let select = document.getElementById(id);
+
+  select.innerHTML = "";
+  console.log("options: ", select.options.length);
+
+  data.forEach((d, i) => {
+    let opt = new Option(d.name, d.id);
+    if (i === 0) opt.selected = true;
+    select.options.add(opt, i);
+  });
+  if (dependant) {
+    populate(dependant.id, dependant.data, dependant.dependant);
+  }
+};
+//load users list
+const loadUsers = (id, users) => {
+  const container = document.getElementById(id);
+  const contChildren = Array.from(container.children);
+  var listContainer;
+  contChildren.forEach((c) => {
+    if (c.id === "list") listContainer = c;
+  });
+  listContainer.innerHTML = "";
+  users.forEach((u) => {
+    const row = document.createElement("div");
+    row.classList.add("col-md-8");
+    row.classList.add("col-lg-8");
+    row.classList.add("col-sm-12");
+    row.classList.add("offset-2");
+    row.classList.add("row");
+    row.id = u.id;
+    const name = document.createElement("span");
+    name.classList.add("col-md-4");
+    name.classList.add("col-lg-4");
+    name.textContent = u.name;
+    row.appendChild(name);
+    const position = document.createElement("span");
+    position.classList.add("col-md-4");
+    position.classList.add("col-lg-4");
+    position.textContent = u.position;
+    row.appendChild(position);
+    const department = document.createElement("span");
+    department.classList.add("col-md-4");
+    department.classList.add("col-lg-4");
+    department.textContent = u.department;
+    row.appendChild(department);
+    listContainer.appendChild(row);
+  });
+};
 const showContent = (hash) => {
   console.log("hash changed: ", hash);
   let content = document.querySelector("#content");
@@ -16,7 +74,46 @@ const showContent = (hash) => {
   });
   contentChildren.forEach((child) => {
     if (child.id === hash.substr(1)) child.style.display = "block";
+    if (child.id === "all_users") {
+      loadUsers(child.id, users);
+    }
+    // if(child.id ==="")
+    if (child.id === "add_user") {
+      let myunits = units.filter((u) => {
+        return u.deptId == departments[0].id;
+      });
+      let mypos = positions.filter((p) => {
+        return p.unit == myunits[0].id;
+      });
+      populate("user_department", departments, {
+        id: "user_unit",
+        data: myunits,
+        dependant: { id: "user_position", data: mypos },
+      });
+      if (child.id === "add_position") {
+        populate("position_dept", departments, null);
+      }
+    }
   });
+  if (hash === "#all_users") {
+    const search = document.querySelector("#search");
+    console.log("search: ", search.id);
+    if (search) {
+      search.addEventListener("change", (e) => {
+        let keyword = e.target.value.toLowerCase();
+        console.log("kw: ", keyword);
+        let filteredUsers = users.filter((u) => {
+          return (
+            u.name.toLowerCase().includes(keyword) ||
+            u.department.toLowerCase().includes(keyword) ||
+            u.position.toLowerCase().includes(keyword)
+          );
+        });
+        console.log("f: ", filteredUsers);
+        loadUsers("all_users", filteredUsers);
+      });
+    }
+  }
 };
 
 if (window.location.pathname.includes("/admin.html")) {
@@ -29,7 +126,142 @@ if (window.location.pathname.includes("/admin.html")) {
     let hash = window.location.hash;
     showContent(hash);
   });
+
+  //submit position form
+  const newPositionForm = document.getElementById("new_position_form");
+  if (newPositionForm) {
+    newPositionForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      let inputs = Array.from(e.target.elements);
+      // window.localStorage.clear();
+      let positions = window.localStorage.positions;
+      console.log("ls: ", positions);
+      if (positions === null || positions === undefined) positions = [];
+      else {
+        positions = JSON.parse(positions);
+      }
+      let position = { id: positions.length };
+      inputs.forEach((i) => {
+        if (i.id === "position_dept") position.deptId = parseInt(i.value);
+        if (i.id === "name") position.name = i.value;
+        if (i.id === "position_unit") position.unit = i.value;
+      });
+      positions.push(position);
+      window.localStorage.setItem("positions", JSON.stringify(positions));
+      console.log("ls2: ", window.localStorage);
+    });
+  }
+
+  //submit user form
+  const newUserForm = document.getElementById("new_user_form");
+  if (newUserForm) {
+    newUserForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      let user = {};
+      if (users === null || users === undefined) {
+        users = [];
+        user.id = users.length;
+      } else {
+        users = users.sort((a, b) => {
+          return a.id - b.id;
+        });
+        user.id = users[users.length - 1].id + 1;
+      }
+      let inputs = Array.from(e.target.elements);
+      inputs.forEach((input) => {
+        if (input.id === "fname") user.name = input.value;
+        if (input.id === "mname" || input.id === "lname")
+          user.name = user.name + " " + input.value;
+        if (input.id === "user_department") {
+          user.deptId = input.options[input.options.selectedIndex].value;
+          user.department = input.options[input.options.selectedIndex].text;
+        }
+        if (input.id === "position") {
+          user.position = input.options[input.options.selectedIndex].text;
+        }
+      });
+    });
+  }
 }
+const groupByArray = (xs, key) => {
+  let result = xs.reduce((rv, x) => {
+    let v = key instanceof Function ? key(x) : x[key];
+    let el = rv.find((r) => r && r.key === v);
+    if (el) {
+      el.values.push(x);
+    } else {
+      rv.push({ key: v, values: [x] });
+    }
+    return rv;
+  }, []);
+
+  return result;
+};
+
+const showActivities = (activities, view = "list") => {
+  var dp = new DayPilot.Scheduler("all_activity");
+  var date = new Date();
+  dp.startDate = new DayPilot.Date(
+    new Date(date.getFullYear(), date.getMonth(), 1)
+  );
+  dp.scale = "Day";
+  dp.timeHeaders = [
+    { groupBy: "Month", format: "MMM yyyy" },
+    { groupBy: "Day", format: "d" },
+  ];
+  dp.days = dp.startDate.daysInMonth();
+  dp.treeEnabled = true;
+  let events = [];
+  activities.map((a) => {
+    let e = a;
+    e.text = a.name;
+    e.start = a.startTime;
+    e.end = a.endTime;
+    return e;
+  });
+  dp.events.list = [
+    {
+      text: "Event 1",
+      id: "5.1",
+      resource: "2",
+      join: 5,
+      start: "2021-02-14T11:00:00",
+      end: "2021-02-16T12:00:00",
+    },
+    {
+      text: "Event 2",
+      id: "5.2",
+      resource: "visual",
+      start: "2021-02-12T11:00:00",
+      end: "2021-02-15T12:00:00",
+    },
+  ];
+  let atvz = [];
+  let assIds = [];
+  console.log("dummy res: ", dummyResources);
+  dummyResources.forEach((dr) => {
+    activitiesData.forEach((ad) => {
+      console.log("ad.resource: ", ad.resource);
+      ad.resource.forEach((r) => {
+        if (dr.id === r.id) {
+          let ai = { id: r.id, children: [] };
+          r.children.forEach((c) => {
+            ai.children.push(c);
+          });
+
+          assIds.push(ai);
+        }
+      });
+    });
+  });
+  console.log("assids: ", assIds);
+  // events;
+  dp.resources = dummyResources;
+  dp.init();
+
+  let gd = groupByArray(activities, "resource");
+  console.log("gd:", gd);
+};
 const initializeSchedule = () => {
   var dp = new DayPilot.Calendar("schedule");
   dp.viewType = "Week";
@@ -43,11 +275,12 @@ const initializeSchedule = () => {
     dp.timeline.push(day);
   }
 
-  dp.events.list = calEvents.map((e) => {
+  dp.events.list = calEvents.map((e, i) => {
     let event = e;
     event.text = e.activity;
     event.start = e.date + "T" + e.startTime;
     event.end = e.date + "T" + e.endTime;
+    // if (i === 2) event.end = "2021-02-14T" + e.endTime;
     return event;
   });
   dp.onEventClicked = (args) => {
@@ -85,6 +318,8 @@ if (window.location.pathname.includes("/dashboard.html")) {
     let hash = window.location.hash;
     if (!hash.length) hash = "#all_activity";
     showContent(hash);
+    if (hash.substr(1) === "all_activity")
+      showActivities(activitiesData, "cal");
     if (hash.substr(1) === "schedule") initializeSchedule();
   });
   window.addEventListener("hashchange", () => {
